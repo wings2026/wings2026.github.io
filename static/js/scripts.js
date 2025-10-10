38495fecfd43df1fa90357985aaa58e624fef916
wings2026 <juuliamm@gmail.com>
2025-10-10 17:00:01 +0100

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
+const section_names = ['home', 'speakers', 'talks', 'schedule', 'participants', 'venue']
 
 
 window.addEventListener('DOMContentLoaded', event => {
